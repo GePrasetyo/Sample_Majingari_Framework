ea32fdaf2b81091ae6ecd4c07c955904fc0c8e36
GePrasetyo <dhimasgppantoja@gmail.com>
2023-10-21 14:47:26 +0200

    Fixed searching local session

--- Sample_Majingari_Framework/Assets/MajinFramework/Scripts/Managers/NetworkGameInstance.cs
+++ Sample_Majingari_Framework/Assets/MajinFramework/Scripts/Managers/NetworkGameInstance.cs
@@ -10,12 +10,16 @@ namespace Majingari.Framework {
         protected override void Tick() {
             base.Tick();
 
-            if (Input.GetKeyDown(KeyCode.S)) {
+            if (Input.GetKeyDown(KeyCode.H)) {
                 ServiceLocator.Resolve<ConnectionHandler>()?.StartGameSesssionHost();
             }
 
-            if (Input.GetKeyDown(KeyCode.J)) {
+            if (Input.GetKeyDown(KeyCode.S)) {
                 ServiceLocator.Resolve<ConnectionHandler>()?.AutoJoinLocalSession();
+            }
+
+            if (Input.GetKeyDown(KeyCode.J)) {
+                ServiceLocator.Resolve<ConnectionHandler>()?.StartGameSesssionClient();
             }
         }
     }
